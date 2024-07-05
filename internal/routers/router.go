40fc824d2b351500e85abd6aa9440b90ab00c457
sx—â_pinkman <sx_pinkman@users.noreply.code.cwoa.net>
2024-07-05 11:00:28 +0800

    release: simple demo deploy

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -5,6 +5,7 @@ import (
 	"awesomeProject1/internal/service/user_services"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func InitRouter() *gin.Engine {
@@ -12,6 +13,7 @@ func InitRouter() *gin.Engine {
 	r.Use(cors.Default())
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
+	r.LoadHTMLGlob("conf/*")
 
 	// UserRouters
 	user := r.Group("/user")
@@ -30,9 +32,19 @@ func InitRouter() *gin.Engine {
 	}
 
 	// DownloadRouters
-	//download := r.Group("/download")
-	//{
-	//	download.GET("/img", file_services.DownloadImg)
-	//}
+	download := r.Group("/download")
+	{
+		download.GET("/img", file_services.DownloadImg)
+		download.GET("/big", file_services.DownloadBigFile)
+	}
+
+	// templates
+	templates := r.Group("/templates")
+	{
+		templates.GET("/index", func(c *gin.Context) {
+			c.HTML(http.StatusOK, "big_file_upload.html", gin.H{})
+		})
+	}
+
 	return r
 }
--- internal/service/file_services/download.go
+++ internal/service/file_services/download.go
@@ -34,3 +34,27 @@ func DownloadImg(c *gin.Context) {
 	resp.Response(http.StatusOK, "下载成功", "")
 	return
 }
+
+func DownloadBigFile(c *gin.Context) {
+	resp := app.Gin{C: c}
+	filepath := c.Query("filepath")
+	file, err := os.Open(filepath)
+
+	if err != nil {
+		resp.Response(http.StatusInternalServerError, "文件不存在", err.Error())
+	}
+	defer file.Close()
+	fileInfo, err := file.Stat()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Cannot get file info"})
+		return
+	}
+
+	c.Header("Content-Disposition", "attachment; filename="+fileInfo.Name())
+	c.Header("Content-Type", "application/octet-stream")
+	c.Header("Content-Length", string(fileInfo.Size()))
+
+	http.ServeContent(c.Writer, c.Request, fileInfo.Name(), fileInfo.ModTime(), file)
+	resp.Response(http.StatusOK, "下载成功", "")
+	return
+}
